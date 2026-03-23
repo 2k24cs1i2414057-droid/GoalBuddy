9581cd427acab138b3742ff8f5a5a3180707d587
HARSH <2k24.cs1i.2414057@gmail.com>
2026-03-24 00:08:07 +0530

    Project start

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home/home.html")
+
+def parent(request):
+    return render(request, "home/parent.html")
+
